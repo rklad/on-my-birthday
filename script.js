ff143b0dd1559dbc780705e1747c2039d3909a7b
rklad <525rkladerman@frhsd.com>
2024-01-30 13:49:31 -0500

    started js framework

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function year1917() {
-
+    document.getElementById('head').innerText = 'Mexico adopts their current Constitution after the Mexican Revolution'
+    document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
+    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
 } 
