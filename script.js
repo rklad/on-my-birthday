34087a98f0f4541dbe55c5e98c132379e859ea1e
rklad <525rkladerman@frhsd.com>
2024-01-31 13:49:21 -0500

    working on styling colors will add actual content later

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function year1917() {
+    let color = '#006341'
+    document.getElementById('news').style.backgroundColor=color
     document.getElementById('head').innerText = 'Mexico adopts their current Constitution after the Mexican Revolution'
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
     document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
