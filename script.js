eaeb9dfe120979192bb8d33b8cfdf3c3a1206d2b
rklad <525rkladerman@frhsd.com>
2024-01-30 23:07:59 -0500

    finished js (need to style and add real text/photos)

--- script.js
+++ script.js
@@ -4,7 +4,51 @@ function year1917() {
     document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/200/300?grayscale'
     document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
     document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
 } 
+
+function year1919(){
+    document.getElementById('head').innerText = 'United Artists founded by Charlie Chaplin, Douglas Fairbanks, Mary Pickford and D. W. Griffith'
+    document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
+    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+}
+
+function year1924(){
+    document.getElementById('head').innerText = 'First ever Winter Olympics are held in Chamonix, France'
+    document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
+    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+}
+
+function year1985(){
+    document.getElementById('head').innerText = 'Cristiano Ronaldo is born'
+    document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
+    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+}
+
+function year2017(){
+    document.getElementById('head').innerText = 'The New England Patriots comeback down 28-3 to win Super Bowl LI'
+    document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
+    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+}
