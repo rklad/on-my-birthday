940d6f5e53b4b205508898ed37c261e0a2e7db78
rklad <525rkladerman@frhsd.com>
2024-01-31 18:33:32 -0500

    continued working on styling and js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 function year1917() {
     let color = '#006341'
+    let border = '#ce1126'
     document.getElementById('news').style.backgroundColor=color
+    document.getElementById('news').style.borderColor=border, border
     document.getElementById('head').innerText = 'Mexico adopts their current Constitution after the Mexican Revolution'
+    document.getElementById('head').style.color = "#ce1126"
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
+    document.getElementById('img1').style.borderColor = border
     document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text1').style.color = "#ce1126"
     document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text2').style.color = "#ce1126"
     document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/200/300?grayscale'
+    document.getElementById('img2').style.borderColor = border
     document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').style.borderColor = border
     document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').style.borderColor = border
+    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('caro').style.borderColor = border
 } 
 
 function year1919(){
+    document.getElementById('news').style.backgroundColor='gray'
     document.getElementById('head').innerText = 'United Artists founded by Charlie Chaplin, Douglas Fairbanks, Mary Pickford and D. W. Griffith'
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
     document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
@@ -23,6 +34,8 @@ function year1919(){
 }
 
 function year1924(){
+    let color = '#002654'
+    document.getElementById('news').style.backgroundColor=color
     document.getElementById('head').innerText = 'First ever Winter Olympics are held in Chamonix, France'
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
     document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
@@ -34,6 +47,8 @@ function year1924(){
 }
 
 function year1985(){
+    let color = '#DA291C'
+    document.getElementById('news').style.backgroundColor=color
     document.getElementById('head').innerText = 'Cristiano Ronaldo is born'
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
     document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
@@ -45,6 +60,8 @@ function year1985(){
 }
 
 function year2017(){
+    let color = '#002244'
+    document.getElementById('news').style.backgroundColor=color
     document.getElementById('head').innerText = 'The New England Patriots comeback down 28-3 to win Super Bowl LI'
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
     document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
