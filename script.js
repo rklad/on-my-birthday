614b28ae69c7d6228472ff605719c082bf6df923
rklad <525rkladerman@frhsd.com>
2024-02-01 15:00:42 -0500

    started content

--- script.js
+++ script.js
@@ -5,69 +5,111 @@ function year1917() {
     document.getElementById('news').style.borderColor=border, border
     document.getElementById('head').innerText = 'Mexico adopts their current Constitution after the Mexican Revolution'
     document.getElementById('head').style.color = "#ce1126"
-    document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
+    document.getElementById('img1').src = 'imgs/mexico.jpg' 
     document.getElementById('img1').style.borderColor = border
-    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+   
+    document.getElementById('text1').innerText = 'On February 5, 1917, Mexico formally adopted its constitution, marking a pivotal moment in the nation\'s history. The Constitution of 1917 emerged in the aftermath of the Mexican Revolution, a period of intense upheaval and social change. Led by President Venustiano Carranza, the drafting of the constitution aimed to address the grievances of the populace and establish a framework for a more just and democratic society. Its provisions included land reforms, labor rights, and the separation of church and state, reflecting the aspirations of the Mexican people for social justice and equitable governance.';
     document.getElementById('text1').style.color = "#ce1126"
-    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('text2').innerText = 'The adoption of the Constitution of 1917 signified Mexico\'s commitment to a new era of governance and national identity. With its emphasis on sovereignty, social welfare, and the rights of the working class, the constitution laid the groundwork for Mexico\'s modern political landscape. By enshrining principles of democracy and equality, it became a symbol of national pride and resilience, shaping the country\'s trajectory and setting the stage for its continued evolution as a vibrant democracy in the years to come.';
     document.getElementById('text2').style.color = "#ce1126"
-    document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('img2').src = 'imgs/mexico2.jpeg'
     document.getElementById('img2').style.borderColor = border
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').style.borderColor = border
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').style.borderColor = border
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').style.borderColor = border
+    document.getElementById('image').src = 'imgs/mexico2.jpeg'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
 } 
 
 function year1919(){
-    document.getElementById('news').style.backgroundColor='gray'
+    let color = 'gray'
+    let border = 'black'
+    document.getElementById('news').style.backgroundColor=color
+    document.getElementById('news').style.borderColor=border, border
     document.getElementById('head').innerText = 'United Artists founded by Charlie Chaplin, Douglas Fairbanks, Mary Pickford and D. W. Griffith'
+    document.getElementById('head').style.color = "black"
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
-    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img1').style.borderColor = border
+    document.getElementById('text1').innerText = 'United Artists, founded on February 5, 1919, emerged as a groundbreaking film studio that revolutionized the way movies were produced and distributed. Spearheaded by four cinematic luminaries - Charlie Chaplin, Douglas Fairbanks, Mary Pickford, and D.W. Griffith - the studio aimed to provide artists with greater creative control over their work and challenge the dominance of the major film corporations. United Artists served as a platform for filmmakers to pursue their artistic visions independently, marking a significant departure from the studio system prevalent at the time.';
+    document.getElementById('text1').style.color = "black"
+    document.getElementById('text2').innerText = 'The founding of United Artists symbolized a turning point in the history of cinema, empowering filmmakers to assert greater artistic autonomy and explore new avenues of storytelling. Through its innovative business model and commitment to artistic freedom, United Artists became a beacon for creativity and innovation in the film industry, producing timeless classics that continue to captivate audiences worldwide. Its legacy endures as a testament to the power of collaboration and the enduring spirit of artistic expression in shaping the cultural landscape of the 20th century and beyond.';
+    document.getElementById('text2').style.color = "black"
     document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('img2').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
 }
 
 function year1924(){
     let color = '#002654'
+    let border = '#ED2939'
     document.getElementById('news').style.backgroundColor=color
-    document.getElementById('head').innerText = 'First ever Winter Olympics are held in Chamonix, France'
+    document.getElementById('news').style.borderColor=border, border
+    document.getElementById('head').innerText = 'First ever Winter Olympics held in Chamonix, France'
+    document.getElementById('head').style.color = "#ED2939"
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
-    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img1').style.borderColor = border
+    document.getElementById('text1').innerText = 'On February 5, 1924, Chamonix, France, played host to the inaugural Winter Olympics, marking a historic moment in the evolution of the modern Olympic Games. The event, officially known as the 1924 International Winter Sports Week, featured a diverse array of cold-weather competitions, including figure skating, ice hockey, and skiing. Athletes from 16 nations gathered in the picturesque Alpine town to showcase their skills and sportsmanship, setting the stage for what would become a beloved tradition celebrated every four years.';
+    document.getElementById('text1').style.color = "#ED2939"
+    document.getElementById('text2').innerText = 'The Chamonix Winter Olympics of 1924 laid the foundation for the global spectacle that the Winter Games have since become. Despite its modest beginnings, the event captured the imagination of sports enthusiasts around the world and highlighted the beauty and thrill of winter sports. With its breathtaking mountain scenery and competitive spirit, the Chamonix Olympics set a precedent for future editions, fostering camaraderie among nations and inspiring generations of athletes to push the boundaries of human achievement on snow and ice.';
+    document.getElementById('text2').style.color = "#ED2939"
     document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('img2').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
 }
 
 function year1985(){
-    let color = '#DA291C'
+    let color = '#ca323f'
+    let border = '#046A38' 
     document.getElementById('news').style.backgroundColor=color
+    document.getElementById('news').style.borderColor=border, border
     document.getElementById('head').innerText = 'Cristiano Ronaldo is born'
+    document.getElementById('head').style.color = "#046A38"
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
-    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img1').style.borderColor = border
+    document.getElementById('text1').innerText = 'Born on February 5, 1985, Cristiano Ronaldo emerged as one of the most iconic and accomplished footballers of his generation. Hailing from Funchal, Madeira, Portugal, Ronaldo exhibited exceptional talent from a young age, captivating audiences with his speed, skill, and athleticism on the pitch. Rising through the ranks of Sporting Lisbon\'s youth academy, he soon caught the attention of top European clubs, ultimately signing with Manchester United in 2003, where he would make his mark as one of the world\'s premier footballers.';
+    document.getElementById('text1').style.color = "#046A38"
+    document.getElementById('text2').innerText = 'Throughout his illustrious career, Cristiano Ronaldo has shattered records and amassed numerous accolades, solidifying his status as a footballing legend. Known for his unparalleled work ethic and dedication to his craft, Ronaldo has won multiple FIFA Ballon d\'Or awards, UEFA Champions League titles, and domestic league championships with various clubs, including Real Madrid and Juventus. Beyond his remarkable achievements on the field, Ronaldo\'s charisma and global appeal have made him a cultural icon, inspiring millions of fans around the world and leaving an indelible mark on the sport of football.';
+    document.getElementById('text2').style.color = "#046A38"
     document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('img2').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
 }
 
 function year2017(){
     let color = '#002244'
+    let border = '#C60C30'
     document.getElementById('news').style.backgroundColor=color
-    document.getElementById('head').innerText = 'The New England Patriots comeback down 28-3 to win Super Bowl LI'
+    document.getElementById('news').style.borderColor=border, border
+    document.getElementById('head').innerText = 'Tom Brady leads New England Patriots to 28-3 comeback in Super Bowl LI'
+    document.getElementById('head').style.color = "#C60C30"
     document.getElementById('img1').src = 'https://picsum.photos/id/237/200/300' 
-    document.getElementById('text1').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-    document.getElementById('text2').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    document.getElementById('img1').style.borderColor = border
+    document.getElementById('text1').innerText = 'On February 5, 2017, Tom Brady orchestrated one of the most memorable comebacks in Super Bowl history, leading the New England Patriots to a stunning 34-28 victory over the Atlanta Falcons in Super Bowl LI. Facing a seemingly insurmountable 28-3 deficit in the third quarter, Brady displayed remarkable poise and resilience, guiding his team with precision and determination. His masterful performance, coupled with the Patriots\' relentless effort, ignited a remarkable turnaround that culminated in the first-ever overtime victory in Super Bowl history.';
+    document.getElementById('text1').style.color = "#C60C30"
+    document.getElementById('text2').innerText = 'Brady\'s leadership and clutch play were on full display as he engineered a remarkable offensive surge, orchestrating touchdown drives and converting crucial plays to chip away at the Falcons\' lead. His connection with key receivers, including Julian Edelman and Danny Amendola, proved instrumental in the Patriots\' comeback effort. Ultimately, Brady\'s unparalleled skill, coupled with his unwavering belief in his team\'s ability to prevail, solidified his legacy as one of the greatest quarterbacks in NFL history and cemented Super Bowl LI as a timeless tale of determination and triumph.';
+    document.getElementById('text2').style.color = "#C60C30"
     document.getElementById('img2').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
-    document.getElementById('caro').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('img2').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
+    document.getElementById('image').src = 'https://picsum.photos/id/237/200/300'
+    document.getElementById('image').style.borderColor = border
 }
